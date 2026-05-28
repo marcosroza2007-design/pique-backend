977cd226b4aeb92b70256adeaa64e4ea6f6c362c
Marcos <marcosroza2007@gmail.com>
2026-05-28 13:24:09 +0200

    añadiendo sync

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 app.get("/", (req, res) => {
 
 res.send("Backend funcionando");
+
+});
+
+app.get("/sync", async (req, res) => {
+
+try {
+
+const response = await axios.get(
+"https://www.thesportsdb.com/api/v1/json/3/eventsnextleague.php?id=4335"
+);
+
+res.json(response.data);
+
+} catch (e) {
+
+res.status(500).send("Error");
+
+}
 
 });
 
