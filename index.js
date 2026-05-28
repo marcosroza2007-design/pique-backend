7b39490bae0fd6c93bf1673637fcacbddc8f586b
Marcos <marcosroza2007@gmail.com>
2026-05-28 13:47:06 +0200

    firebase conectado

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const axios = require("axios");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./pique-9ca6a-firebase-adminsdk-fbsvc-6dc238b5f6.json");
+
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
 
 const app = express();
 
@@ -14,12 +23,35 @@ app.get("/sync", async (req, res) => {
 try {
 
 const response = await axios.get(
-"https://www.thesportsdb.com/api/v1/json/3/eventsnextleague.php?id=4335"
+"https://www.thesportsdb.com/api/v1/json/3/eventsnextleague.php?id=4328"
 );
 
-res.json(response.data);
+const eventos = response.data.events;
+
+if (!eventos) {
+
+return res.send("Sin eventos");
+
+}
+
+for (const evento of eventos) {
+
+await db.collection("eventos").doc(evento.idEvent).set({
+
+equipoA: evento.strHomeTeam,
+equipoB: evento.strAwayTeam,
+fecha: evento.strTimestamp,
+competicion: "Premier League",
+
+});
+
+}
+
+res.send("Eventos guardados");
 
 } catch (e) {
+
+console.log(e);
 
 res.status(500).send("Error");
 
